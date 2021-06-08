e17697a1382f5f87d6baa9ddf13f48b1fd6a1551
庞家伟 <15501149646@163.com>
2021-06-08 16:44:25 +0800

    测试hotfix

--- src/main/java/com/atguigu/TestGit.java
+++ src/main/java/com/atguigu/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("111111111");
         System.out.println("master");
+        System.out.println("hotfix");
     }
 }
